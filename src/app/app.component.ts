37626c2eaa1cb1f71e43faa90b8768117dc59c22
Vikramjeet Singh <singhvikramjeet786@gmail.com>
2021-12-29 14:48:47 +0530

    added translate pipe for translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'i18nJsonPoc & dynamic-translations';
+  titleVar = 'i18nJsonPoc & dynamic-translations';
 }
